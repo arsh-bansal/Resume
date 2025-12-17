c8db3dfe1386f87ba8e8bd176267a810f0e86eff
aban0052 <aban0052@student.monash.edu>
2025-12-17 11:31:59 +1100

    Fix build error: remove optimizeCss experimental feature

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200],
